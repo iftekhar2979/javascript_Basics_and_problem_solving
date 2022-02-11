73dcc241b96b9ec56c572d1ffba0c996d5478f8e
unknown <iftekharsalmin@gmail.com>
2022-02-11 10:19:05 +0600

    array slice splice and join

---
+++ arraySlice.js
@@ -0,0 +1,3 @@
+const nums=[1,2,3,4,5,6,7,8]
+const numberSlice=nums.slice(2,5);
+console.log(numberSlice);
---
+++ arrraySplice.js
@@ -0,0 +1,4 @@
+const nums=[1,2,3,4,5,6,7,8];
+const remove=nums.splice(1,4, 78, 58);
+console.log(remove);
+console.log(nums);
