e5e932efcae24fc6fbd93d12544de06c9a83facf
unknown <iftekharsalmin@gmail.com>
2022-02-14 18:27:09 +0600

    bind in object methods commit

---
+++ objectBind.js
@@ -0,0 +1,54 @@
+const person={
+    firstName:'Iftekhar',
+    LastName:"salmin",
+    salary:15000,
+
+    // Object method declare
+    getfullName :function(){
+        console.log(this.firstName,this.LastName);
+    },
+    // second object method declare
+
+    chargeBill:function(amount){
+      console.log(this)
+        this.salary=this.salary-amount;
+        return this.salary;
+    }
+}
+
+// binding an object method to new objects 
+// first example
+
+const heroAlam={
+    firstName:'Hero',
+    lastName:'Alam',
+    salary:30000 
+}
+const heroChargeBill=person.chargeBill.bind(heroAlam);
+heroChargeBill(2000);
+heroChargeBill(1000);
+
+// second example
+
+ const seraAlam={
+    firstName:'Sera',
+    lastName:'Alam',
+    salary:3000 
+}
+
+const seraChargeBill=person.chargeBill.bind(seraAlam);
+seraChargeBill(200);
+console.log(seraAlam);
+
+// third example
+
+const deraAlam={
+    firstName:'Dera',
+    lastName:'Alam',
+    salary:4000
+}
+const deraChargeBill=person.chargeBill.bind(deraAlam);
+ 
+ deraChargeBill(500);
+ console.log(deraAlam);
+
